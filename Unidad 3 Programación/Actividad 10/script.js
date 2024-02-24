3d68964287edc899f8bd65db9bc17a511e9bd361
emasmas0407 <151036181+emasmas0407@users.noreply.github.com>
2024-02-24 12:34:47 +0100

    Corrección

--- Unidad 3 Programación/Actividad 10/script.js
+++ Unidad 3 Programación/Actividad 10/script.js
@@ -1,15 +1,8 @@
 function calificar() {
     let alumnos = ["Juan", "Pedro", "María", "Pablo"];
     let notas = [];
-    let nota;
     for (let i = 0; i < 4; i++) {
-        nota = parseInt(prompt("¿Qué nota le pones a " + alumnos[i] + "?"));
-        if (nota < 0 || nota > 10) {
-            alert("Error: La nota ingresada no es válida.");
-            i--;
-            continue;
-        }
-        notas.push(nota);
+        notas[i] = parseInt(prompt("¿Qué nota le pones a " + alumnos[i] + "?"));
     }
     let resultado = "";
     for (let i = 0; i < 4; i++) {
